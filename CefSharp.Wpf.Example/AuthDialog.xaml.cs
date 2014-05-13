ae597fd5774bff98ac83ced34b0c25dea622809e
amaitland <alex@maitlandcomputing.com.au>
2014-05-13 15:35:20 +1000

    Add AuthDialog for Username/password collection

---
+++ CefSharp.Wpf.Example/AuthDialog.xaml.cs
@@ -0,0 +1,58 @@
+﻿// Copyright © 2010-2014 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CefSharp.Example;
+
+namespace CefSharp.Wpf.Example
+{
+	/// <summary>
+	/// Interaction logic for AuthDialog.xaml
+	/// </summary>
+	public partial class AuthDialog : Window, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private string userName;
+		public string UserName
+		{
+			get { return userName; }
+			set { PropertyChanged.ChangeAndNotify(ref userName, value, () => UserName); }
+		}
+
+		private string password;
+		public string Password
+		{
+			get { return password; }
+			set { PropertyChanged.ChangeAndNotify(ref password, value, () => Password); }
+		}
+
+		public AuthDialog()
+		{
+			InitializeComponent();
+
+			CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseDialog));
+
+			DataContext = this;
+		}
+
+		private void CloseDialog(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (e.Parameter != null)
+			{
+				DialogResult = bool.Parse(e.Parameter.ToString());
+			}
+			Close();
+		}
+
+		private void PasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
+		{
+			var passwordBox = (PasswordBox)sender;
+			Password = passwordBox.Password;
+		}
+	}
+}

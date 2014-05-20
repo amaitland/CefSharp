ab6511b764115b93b4d9d8baf4d881653a2fc1e8
amaitland <alex@maitlandcomputing.com.au>
2014-05-20 17:21:43 +1000

    Change IWebBrowser.LoadError to have proper EventArgs (LoadErrorEventArgs)

--- CefSharp.Wpf/WebView.cs
+++ CefSharp.Wpf/WebView.cs
@@ -1011,7 +1011,7 @@ namespace CefSharp.Wpf
             var handler = LoadError;
             if (handler != null)
             {
-                handler(url, errorCode, errorText);
+                handler(this, new LoadErrorEventArgs(url, errorCode, errorText));
             }
         }
 

99074cdbbe5cf8a7ec1b67665653604843e43d35
amaitland <alex@maitlandcomputing.com.au>
2014-05-20 17:44:36 +1000

    Make print public

--- CefSharp.Wpf/WebView.cs
+++ CefSharp.Wpf/WebView.cs
@@ -922,7 +922,7 @@ namespace CefSharp.Wpf
             managedCefBrowserAdapter.Reload(ignoreCache);
         }
 
-        private void Print()
+        public void Print()
         {
             managedCefBrowserAdapter.Print();
         }

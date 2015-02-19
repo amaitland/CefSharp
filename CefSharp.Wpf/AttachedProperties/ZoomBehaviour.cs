b22054ec04daafd2d97ce7464eb79645034f1fee
amaitland <alex@maitlandcomputing.com.au>
2015-02-19 18:14:52 +1000

    Untabify

--- CefSharp.Wpf/AttachedProperties/ZoomBehaviour.cs
+++ CefSharp.Wpf/AttachedProperties/ZoomBehaviour.cs
@@ -7,59 +7,59 @@ using System.Windows.Threading;
 
 namespace CefSharp.Wpf.AttachedProperties
 {
-	public static class ZoomBehaviour
-	{
-		public static readonly DependencyProperty AutoZoomProperty = DependencyProperty.RegisterAttached("AutoZoom", typeof(bool), typeof(ZoomBehaviour), new PropertyMetadata(false, OnAutoZoomChanged));
+    public static class ZoomBehaviour
+    {
+        public static readonly DependencyProperty AutoZoomProperty = DependencyProperty.RegisterAttached("AutoZoom", typeof(bool), typeof(ZoomBehaviour), new PropertyMetadata(false, OnAutoZoomChanged));
 
-		public static bool GetAutoZoom(DependencyObject obj)
-		{
-			return (bool)obj.GetValue(AutoZoomProperty);
-		}
+        public static bool GetAutoZoom(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoZoomProperty);
+        }
 
-		public static void SetAutoZoom(DependencyObject obj, bool value)
-		{
-			obj.SetValue(AutoZoomProperty, value);
-		}
+        public static void SetAutoZoom(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoZoomProperty, value);
+        }
 
-		private static void OnAutoZoomChanged(object sender, DependencyPropertyChangedEventArgs e)
-		{
-			var browser = (ChromiumWebBrowser)sender;
+        private static void OnAutoZoomChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var browser = (ChromiumWebBrowser)sender;
 
-			var autoZoom = (bool)(e.NewValue);
-			if (autoZoom)
-			{
-				browser.Unloaded += BrowserUnloaded;
-				browser.FrameLoadStart += BrowserFrameLoadStart;
-			}
-			else
-			{
-				browser.Unloaded -= BrowserUnloaded;
-				browser.FrameLoadStart -= BrowserFrameLoadStart;
-			}
-		}
+            var autoZoom = (bool)(e.NewValue);
+            if (autoZoom)
+            {
+                browser.Unloaded += BrowserUnloaded;
+                browser.FrameLoadStart += BrowserFrameLoadStart;
+            }
+            else
+            {
+                browser.Unloaded -= BrowserUnloaded;
+                browser.FrameLoadStart -= BrowserFrameLoadStart;
+            }
+        }
 
-		/// <summary>
-		/// Browser Unloaded - Cleanup Event handlers
-		/// </summary>
-		/// <param name="sender">sender</param>
-		/// <param name="e">event args</param>
-		private static void BrowserUnloaded(object sender, RoutedEventArgs e)
-		{
-			var browser = (ChromiumWebBrowser)sender;
+        /// <summary>
+        /// Browser Unloaded - Cleanup Event handlers
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event args</param>
+        private static void BrowserUnloaded(object sender, RoutedEventArgs e)
+        {
+            var browser = (ChromiumWebBrowser)sender;
 
-			browser.Unloaded -= BrowserUnloaded;
-			browser.FrameLoadStart -= BrowserFrameLoadStart;
-		}
+            browser.Unloaded -= BrowserUnloaded;
+            browser.FrameLoadStart -= BrowserFrameLoadStart;
+        }
 
-		private static void BrowserFrameLoadStart(object sender, FrameLoadStartEventArgs e)
-		{
-			var browser = (ChromiumWebBrowser)sender;
+        private static void BrowserFrameLoadStart(object sender, FrameLoadStartEventArgs e)
+        {
+            var browser = (ChromiumWebBrowser)sender;
 
-			browser.UiThreadRunAsync(() =>
-			{
-				var autoZoomLevel = browser.CalculateAutoZoom();
-			    browser.SetZoomLevel(autoZoomLevel);
-			}, DispatcherPriority.Render);
-		}
-	}
+            browser.UiThreadRunAsync(() =>
+            {
+                var autoZoomLevel = browser.CalculateAutoZoom();
+                browser.SetZoomLevel(autoZoomLevel);
+            }, DispatcherPriority.Render);
+        }
+    }
 }

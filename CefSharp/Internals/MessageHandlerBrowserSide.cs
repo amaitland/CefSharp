c96044277c440755e55982741f8dbeecbea69b59
amaitland <alex@maitlandcomputing.com.au>
2015-07-21 12:15:17 +1000

    Move DeserializeV8Object into MessagingExtensions

--- CefSharp/Internals/MessageHandlerBrowserSide.cs
+++ CefSharp/Internals/MessageHandlerBrowserSide.cs
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CefSharp.Internals
@@ -41,7 +40,7 @@ namespace CefSharp.Internals
 
                     if (success)
                     {
-                        response.Result = DeserializeV8Object(argList, 3, new JavascriptCallbackFactory(pendingTaskRepository, browser));
+                        response.Result = argList.DeserializeV8Object(3, new JavascriptCallbackFactory(pendingTaskRepository, browser));
                     }
                     else
                     {
@@ -78,96 +77,6 @@ namespace CefSharp.Internals
         {
             //TODO: Implement
             base.DoDispose(isDisposing);
-        }
-        
-        //TODO: Turn this into an extension method
-        private object DeserializeV8Object(IListValue list, int index, IJavascriptCallbackFactory javascriptCallbackFactory)
-        {
-            var type = list.GetType(index);
-
-            if (type == CefValueType.Bool)
-            {
-                return list.GetBool(index);
-            }
-            
-            if (type == CefValueType.Int)
-            {
-                return list.GetInt(index);
-            }
-            
-            if (type == CefValueType.Double)
-            {
-                return list.GetDouble(index);
-            }
-
-            if (type == CefValueType.String)
-            {
-                return list.GetString(index);
-            }
-
-            if (type == CefValueType.List)
-            {
-                var subList = list.GetList(index);
-                var array = new List<Object>((int)subList.GetSize());
-                for (var i = 0; i < array.Capacity; i++)
-                {
-                    array.Add(DeserializeV8Object(subList, i, javascriptCallbackFactory));
-                }
-                return array;
-            }
-
-            if (type == CefValueType.Dictionary)
-            {
-                throw new NotImplementedException();
-                //var dict = new Dictionary<String, Object>();
-                //var subDict = list.GetDictionary(index);
-                //std::vector<CefString> keys;
-                //subDict.GetKeys(keys);
-
-                //for (auto i = 0; i < keys.size(); i++)
-                //{
-                //	dict->Add(StringUtils::ToClr(keys[i]), DeserializeV8Object(subDict, keys[i], javascriptCallbackFactory));
-                //}
-
-                //result = dict;
-            }
-
-            if (type == CefValueType.Binary)
-            {
-                var binary = list.GetBinary(index);
-
-                var t = (PrimitiveType)binary[0];
-
-                if (t == PrimitiveType.Int64)
-                {
-                    return BitConverter.ToInt64(binary, 1);
-                }
-
-                if (t == PrimitiveType.CefTime)
-                {
-                    var epoch = BitConverter.ToDouble(binary, 1);
-
-                    return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(epoch).ToLocalTime();
-                }
-
-                if (t == PrimitiveType.JsCallback)
-                {
-                    var browserId = new byte[sizeof(int)];
-
-                    //Copy the middle bytes out that represent Browser Id
-                    Array.Copy(binary, 1, browserId, 0, browserId.Length);
-
-                    var result = new JavascriptCallback
-                    {
-                        BrowserId = BitConverter.ToInt32(browserId, 0),
-                        Id = BitConverter.ToInt64(binary, 1 + sizeof (int))
-                    };
-
-                    return javascriptCallbackFactory.Create(result);
-                }
-            }
-
-            throw new NotImplementedException();
         }
     }
 }
--- CefSharp/Internals/MessagingExtensions.cs
+++ CefSharp/Internals/MessagingExtensions.cs
@@ -2,6 +2,9 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+using System;
+using System.Collections.Generic;
+
 namespace CefSharp.Internals
 {
     public static class MessagingExtensions
@@ -125,6 +128,95 @@ namespace CefSharp.Internals
             }
 
             return result;
+        }
+
+        public static object DeserializeV8Object(this IListValue list, int index, IJavascriptCallbackFactory javascriptCallbackFactory)
+        {
+            var type = list.GetType(index);
+
+            if (type == CefValueType.Bool)
+            {
+                return list.GetBool(index);
+            }
+
+            if (type == CefValueType.Int)
+            {
+                return list.GetInt(index);
+            }
+
+            if (type == CefValueType.Double)
+            {
+                return list.GetDouble(index);
+            }
+
+            if (type == CefValueType.String)
+            {
+                return list.GetString(index);
+            }
+
+            if (type == CefValueType.List)
+            {
+                var subList = list.GetList(index);
+                var array = new List<Object>((int)subList.GetSize());
+                for (var i = 0; i < array.Capacity; i++)
+                {
+                    array.Add(subList.DeserializeV8Object(i, javascriptCallbackFactory));
+                }
+                return array;
+            }
+
+            if (type == CefValueType.Dictionary)
+            {
+                throw new NotImplementedException();
+                //var dict = new Dictionary<String, Object>();
+                //var subDict = list.GetDictionary(index);
+                //std::vector<CefString> keys;
+                //subDict.GetKeys(keys);
+
+                //for (auto i = 0; i < keys.size(); i++)
+                //{
+                //	dict->Add(StringUtils::ToClr(keys[i]), DeserializeV8Object(subDict, keys[i], javascriptCallbackFactory));
+                //}
+
+                //result = dict;
+            }
+
+            if (type == CefValueType.Binary)
+            {
+                var binary = list.GetBinary(index);
+
+                var t = (PrimitiveType)binary[0];
+
+                if (t == PrimitiveType.Int64)
+                {
+                    return BitConverter.ToInt64(binary, 1);
+                }
+
+                if (t == PrimitiveType.CefTime)
+                {
+                    var epoch = BitConverter.ToDouble(binary, 1);
+
+                    return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(epoch).ToLocalTime();
+                }
+
+                if (t == PrimitiveType.JsCallback)
+                {
+                    var browserId = new byte[sizeof(int)];
+
+                    //Copy the middle bytes out that represent Browser Id
+                    Array.Copy(binary, 1, browserId, 0, browserId.Length);
+
+                    var result = new JavascriptCallback
+                    {
+                        BrowserId = BitConverter.ToInt32(browserId, 0),
+                        Id = BitConverter.ToInt64(binary, 1 + sizeof(int))
+                    };
+
+                    return javascriptCallbackFactory.Create(result);
+                }
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
